3cd50602a34bb3ad8cd74c07fa9581a376087072
locdd <locdd@vng.com.vn>
2023-12-10 19:40:03 +0700

    Add MVC

--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ const exphbs = require('express-handlebars');
 const app = express();
 const port = 9000;
 
+const route = require('./routes');
+
 // static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// body-parser
+app.use(express.urlencoded({
+  extended: true,
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 
 // Template engine
 app.engine('hbs', exphbs.create({
@@ -19,9 +27,10 @@ app.engine('hbs', exphbs.create({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+// Home, search, contact
+
+// Route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
