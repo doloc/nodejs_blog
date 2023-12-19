ff4327a64e3ae3864869838ffb5d72b66df6c5b8
locdd <locdd@vng.com.vn>
2023-12-19 13:02:48 +0700

    update config db mongo prod

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/f8_education_dev');
+        // await mongoose.connect('mongodb://localhost:27017/f8_education_dev');
+        await mongoose.connect('mongodb+srv://dolocnd:thuhoai123@education.mfgqebh.mongodb.net/f8_education_prod');
         console.log('Connect successfully!!!');
     } catch (error) {
         console.log('Connect failure!!!');
